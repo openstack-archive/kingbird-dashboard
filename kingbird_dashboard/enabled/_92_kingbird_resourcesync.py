4b4e5657fc502232bc79cfe899500939479dc316
Goutham Pratapa <pratapagoutham@gmail.com>
2018-01-12 18:36:14 +0530

    Add enabled files in Kingbird-Dashboard.
    These enabled files are required to create panel and
    panel group in a horizon the steps are mentioned in README.rst
    
    Change-Id: I65af14d28d8cfa795a0ebbe02e008f2d34ccc995

---
+++ kingbird_dashboard/enabled/_92_kingbird_resourcesync.py
@@ -0,0 +1,9 @@
+# The slug of the panel to be added to HORIZON_CONFIG. Required.
+PANEL = 'resource_management'
+# The slug of the dashboard the PANEL associated with. Required.
+PANEL_DASHBOARD = 'kingbird'
+# The slug of the panel group the PANEL is associated with.
+PANEL_GROUP = 'default'
+
+# Python panel class of the PANEL to be added.
+ADD_PANEL='kingbirddashboard.resource_management.panel.ResourceManagement'
